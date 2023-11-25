228397a0f7254ce93a1d9285eec35239df2ce272
Bill Drol <billdrol64@gmail.com>
2023-11-25 07:40:35 -0500

    Exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function GuessInput() {
+  const [text, setText] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (text.length !== 5) {
+      alert("Guess must be 5 characters long.");
+    } else {
+      console.log(text);
+      setText("");
+    }
+  }
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => handleSubmit(event)}
+    >
+      <label htmlFor="guess-input">Enter guess (5 letters, A to Z only):</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={text}
+        maxLength="5"
+        pattern="[A-Za-z]{5}"
+        onChange={(event) => {
+          setText(event.target.value.toUpperCase());
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
